25e3a6361890b50a7330a80772a6f8364130a96e
mckennaj6 <mckennaj6@wit.edu>
2019-07-18 16:43:32 -0400

    Update code.py
    added some stuff for instruction fetch

--- code.py
+++ code.py
@@ -1,11 +1,11 @@
 #code for stupid stuff
 
 #step 1:
-#class binary
+#class binary - to binary (for numbers)
 
 #class Rformat
 class Rformat:
-    def __init__(self,inctruction):
+    def __init__(self,inctruc):
     self.opcode = instruction.digits(31,21)
     self.rm =instruction.digits(20,16)
     self.shmnt = instruction.digits(15,10)
@@ -14,7 +14,7 @@ class Rformat:
 
 #class Iformat
 class Iformat:
-    def __init__(self,instruction):
+    def __init__(self,instruc):
     self.opcode = instruction.digits(31,22)
     self.immediate = instruction.digits(21,10)
     self.rn = instruction.digits(9,5)
@@ -22,7 +22,7 @@ class Iformat:
 
 #class Dformat
 class Dformat:
-    def __init__(self,instruction):
+    def __init__(self,instruc):
     self.opcode = instruction.digits(31,21)
     self.address = instruction.digits(20,12)
     self.op2 = instruction.digits(11,10)
@@ -31,7 +31,7 @@ class Dformat:
 
 #class CBformat
 class CBformat:
-    def __init__(self,instruction)
+    def __init__(self,instruc)
     self.opcode = instruction.digits(31,24)
     self.address = instruction.digits(23,5)
     self.rt = instruction.digits(4,0)
@@ -44,6 +44,41 @@ class CBformat:
 #instruction decode
 
 #if statement for each option, add, sub, addi
+#ib (instriction bit)
+
+if self.opcode == 1112:
+    self.instruction = ("ADD")
+    self.alucontrol = 0010
+    #add
+    pass
+elif self.opcode == 1624:
+    #for SUB
+    pass
+elif self.opcode == 580:
+    #addi
+    pass
+elif self.opcode == 836:
+    #subi
+    pass
+elif self.opcode == 1986:
+    #ldur
+elif self.opcode == 1984:
+    #stur
+    pass
+elif self.opcode == 1104:
+    #and
+    pass
+elif self.opcode == 1360:
+    #orr
+    pass
+elif self.opcode > 1439 && self.opcode < 1448:
+    #cbz
+    pass
+elif self.opcode > 159 && self.opcode < 192:
+    #b
+    pass
+else:
+    pass
 
 #register fetch
 
